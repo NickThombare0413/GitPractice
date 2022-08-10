8eb0eb784778b83e2cc5cb0bf73a2e73fc7f951b
Niketan <nickthombare@gmail.com>
2022-08-10 23:20:47 +0530

    first commit

--- Demo.java
+++ Demo.java
@@ -2,10 +2,6 @@ public class Demo
 {
    public static void main(String [] args){
 
-                  public void cd1()
-               {
-                  int xyz=10;
-                   int x=56;
-               }
+               
       }
 }
